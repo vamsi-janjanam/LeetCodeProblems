c24628ab9c4b94389406fdef2aab21eb6126fec4
vamsi95 <vamsijanjanam95@gmail.com>
2022-04-18 19:46:19 -0500

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,17 @@
+#TwoSum.py
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return[i,j]
+        complementmap = dict()
+        
+        for i in range(len(nums)):
+            num = nums[i]
+            complement = target-num
+            if nums[i] in complementmap:
+                return[complementmap[num],i]
+            else:
+                complementmap[complement] = i
